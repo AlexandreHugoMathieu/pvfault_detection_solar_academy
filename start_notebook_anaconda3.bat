9ff9d3b06c059a8a4a47b0781e440df0f3bed572
AlexandreHugoMathieu <mathieu.al@laposte.net>
2023-12-07 11:47:42 +0100

    Correct the start up file

--- start_notebook_anaconda3.bat
+++ start_notebook_anaconda3.bat
@@ -1,5 +1,5 @@
 C:\anaconda3\python -m pip install virtualenv
-C:\anaconda3\python -m python -m virtualenv solar_env
+C:\anaconda3\python -m virtualenv solar_env
 call solar_env\Scripts\activate
 C:\anaconda3\python -m pip install -r requirements.txt
 C:\anaconda3\python -m ipykernel install --name=solarkernel
